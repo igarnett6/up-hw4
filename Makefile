c0ebd6f1f0fcd69971f77d0943509e70c18a4601
axion <ig907@nyu.edu>
2021-03-18 19:55:28 -0400

    updated io redirection (still non-functional)

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 shell: shell.c
-	gcc -Wall -std=c99 -o shell shell.c
+	gcc -Wall -std=c99 -o shell shell.c -D _XOPEN_SOURCE=500
 
 clean:
 	rm -f shell
--- shell.c
+++ shell.c
@@ -15,11 +15,12 @@
 #include <sys/wait.h> //wait
 #include <stdlib.h> //?malloc
 #include <limits.h> //realpath
+#include <fcntl.h> //O_RDONLY
+
 
 int main()
 {
     while(true) {
-        int redirIO = 0;
         char *sourceFile;
         char pwd[4096];
         realpath(".", pwd);
@@ -27,32 +28,31 @@ int main()
         printf("╰─(╯°益°)╯彡┻━┻ ");
         char *command = calloc(1, 4096);
         fgets(command, 4096, stdin);
+        int len = strlen(command);
+        if (command[len-1] == '\n') command[len-1] = '\0';
         if((sourceFile = strstr(command, " < ")) != NULL){
-          redirIO = 1;
-          sourceFile = sourceFile+3;
+          sourceFile = sourceFile+3; //this only works @end, change to strtok
           close(0);
-          int fd = open(sourceFile, "O_RDONLY");
-          if(fd != 0){
-            fprintf(stderr, "failed to open %s \0\n", sourceFile);
-            printf("fd:\t%d\n", fd);
+          int fd = open(sourceFile, O_RDONLY);
+          if(fd == -1){
+            perror("Failed to open \"sourceFile\"");
             exit(1);
           }
-          printf("fd:\t%d\n", fd);
-          fgets(command, 4096, stdin);
-          printf("command: %s\n", command);
-          continue;
+          printf("\nfd:\t%d\n", fd);
+          if((command = fgets(command, 4096, stdin)) == NULL){
+            perror("fgets returned NULL from stdin");
+          }
+          if (command[len-1] == '\n') command[len-1] = '\0';
+          close(fd);
         }
-        int len = strlen(command);
-        int changeDir = 0;
+        // printf("command:\t%s\n", command);
         if((strncmp(command, "exit", 4)) == 0){
           exit(0);
         }
+        int changeDir = 0; //bool for if cd in stdin
         if((strncmp(command, "cd", 2)) == 0){
           changeDir = 1;
         }
-        // printf("redirIO:\t%d\n", redirIO);
-        // printf("sourceFile: %s\n", sourceFile);
-        if (command[len-1] == '\n') command[len-1] = '\0';
         if(changeDir == 1){
           strtok(command, " ");
           char *newDir = strtok(NULL, " ");
@@ -62,34 +62,24 @@ int main()
         if(fork() == 0){
           char *token = calloc(1, 4096);
           token = strtok(command, " ");
-          // else{
-          // }
           char *args[4096];
           args[0] = token;
           int i = 1;
           while((token = strtok(NULL, " ")) != NULL){
             args[i] = token;
+            int argLen = strlen(args[i]);
+            if (args[i][argLen-1] == '\n') args[i][argLen-1] = '\0';
             i++;
           }
           execvp(args[0], args);
           perror("Failed to exec");
           free(token);
-          // if(redirIO == 1){
-          //   close(0);
-          //   int fd = open(sourceFile, "O_RDONLY");
-          //   if(fd != 0){
-          //     _exit(1);
-          //   }
-            // fputs(command, stdin);
-            // write(1, "\ncommand: ", 10);
-            // write(1, command, 20);
-            // write(1, "\n", 1);
-          // }
           _exit(0);
         }
         else{
-          wait(NULL);
-          // perror("No children");
+          if((wait(NULL)) == -1){
+            perror("No children");
+          }
         }
         free(command);
     }
